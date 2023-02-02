7eb140022abdc3d73dc1cb188834ac22af6ebacd
Thanadol Chitthamlerd <thanadolps@gmail.com>
2023-02-02 16:32:01 +0700

    Create controller

--- routes/hospitals.js
+++ routes/hospitals.js
@@ -1,26 +1,9 @@
 const express = require('express');
+const { getHospitals, getHospital, createHospital, updateHospital, deleteHospital } = require('../controllers/hospitals');
+
 const router = express.Router();
 
-const app = express();
-
-router.get('/', (req, res) => {
-    res.status(200).json({ success: true, msg: 'Show all hospitals' });
-});
-
-router.get('/:id', (req, res) => {
-    res.status(200).json({ success: true, msg: `Show hospital ${req.params.id}` });
-})
-
-router.post('/', (req, res) => {
-    res.status(200).json({ success: true, msg: 'Create new hospital' });
-});
-
-router.put('/:id', (req, res) => {
-    res.status(200).json({ success: true, msg: `Update hospital ${req.params.id}` });
-});
-
-router.delete('/:id', (req, res) => {
-    res.status(200).json({ success: true, msg: `Delete hospital ${req.params.id}` });
-});
+router.route('/').get(getHospitals).post(createHospital);
+router.route('/:id').get(getHospital).put(updateHospital).delete(deleteHospital);
 
 module.exports=router;

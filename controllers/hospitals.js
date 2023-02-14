9224c60d1080d0e9c3b74cde39243095108794dc
Thanadol Chitthamlerd <thanadolps@gmail.com>
2023-02-15 00:05:17 +0700

    🎨 Autoformat

--- controllers/hospitals.js
+++ controllers/hospitals.js
@@ -6,13 +6,15 @@ const Hosptial = require("../models/Hosptial");
  * @access  Public
  */
 exports.getHospitals = async (req, res, next) => {
-    try {
-        const hospitals = await Hosptial.find();
-        res.status(200).json({ success: true, count: hospitals.length, data: hospitals });
-    } catch(err) {
-        res.status(400).json({ success: false });
-    }
-}
+  try {
+    const hospitals = await Hosptial.find();
+    res
+      .status(200)
+      .json({ success: true, count: hospitals.length, data: hospitals });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
+};
 
 /**
  * @desc    Get single hospital
@@ -20,16 +22,16 @@ exports.getHospitals = async (req, res, next) => {
  * @access  Public
  */
 exports.getHospital = async (req, res, next) => {
-    try {
-        const hospital = await Hosptial.findById(req.params.id);
-        if (!hospital) {
-            return res.status(400).json({ success: false });
-        }
-        res.status(200).json({ success: true, data: hospital });
-    } catch(err) {
-        res.status(400).json({ success: false });
+  try {
+    const hospital = await Hosptial.findById(req.params.id);
+    if (!hospital) {
+      return res.status(400).json({ success: false });
     }
-}
+    res.status(200).json({ success: true, data: hospital });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
+};
 
 /**
  * @desc    Create new hospital
@@ -37,9 +39,9 @@ exports.getHospital = async (req, res, next) => {
  * @access  Private
  */
 exports.createHospital = async (req, res, next) => {
-    const hospital = await Hosptial.create(req.body);
-    res.status(201).json({ success: true, data: hospital });
-}
+  const hospital = await Hosptial.create(req.body);
+  res.status(201).json({ success: true, data: hospital });
+};
 
 /**
  * @desc    Update hospital
@@ -47,21 +49,21 @@ exports.createHospital = async (req, res, next) => {
  * @access  Private
  */
 exports.updateHospital = async (req, res, next) => {
-    try {
-        const hospital = await Hosptial.findByIdAndUpdate(req.params.id, req.body, {
-            new: true,
-            runValidators: true
-        });
+  try {
+    const hospital = await Hosptial.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
 
-        if (!hospital) {
-            return res.status(400).json({ success: false });
-        }
+    if (!hospital) {
+      return res.status(400).json({ success: false });
+    }
 
-        res.status(200).json({ success: true, data: hospital });
-    } catch(err) {
-        res.status(400).json({ success: false });
-    } 
-}
+    res.status(200).json({ success: true, data: hospital });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
+};
 
 /**
  * @desc    Delete hospital
@@ -69,13 +71,13 @@ exports.updateHospital = async (req, res, next) => {
  * @access  Private
  */
 exports.deleteHospital = async (req, res, next) => {
-    try {
-        const hospital = await Hosptial.findByIdAndDelete(req.params.id);
-        if (!hospital) {
-            return res.status(400).json({ success: false });
-        }
-        res.status(200).json({ success: true, data: {} });
-    } catch(err) {
-        res.status(400).json({ success: false });
-    } 
-}
+  try {
+    const hospital = await Hosptial.findByIdAndDelete(req.params.id);
+    if (!hospital) {
+      return res.status(400).json({ success: false });
+    }
+    res.status(200).json({ success: true, data: {} });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
+};
--- server.js
+++ server.js
@@ -1,30 +1,34 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
-
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
 // Load env vars
-dotenv.config({ path: './config/config.env' });
+dotenv.config({ path: "./config/config.env" });
 
 // Connect to database
 connectDB();
 
 // Route files
-const hospitals = require('./routes/hospitals');
+const hospitals = require("./routes/hospitals");
+const auth = require("./routes/auth");
 
-const app=express();
+const app = express();
 
 // Mount routers
 app.use(express.json());
-app.use('/api/v1/hospitals', hospitals);
+app.use("/api/v1/hospitals", hospitals);
+app.use("/api/v1/auth", auth);
 
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
 
 // Handle unhandled promise rejections
-process.on('unhandledRejection', (err, promise) => {
-    console.log(`Error: ${err.message}`);
-    // Close server & exit process
-    server.close(() => process.exit(1));
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & exit process
+  server.close(() => process.exit(1));
 });

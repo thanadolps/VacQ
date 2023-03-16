f82a77ddb714e05950241a0ed5b6c38dba9294e4
Thanadol Chitthamlerd <thanadolps@gmail.com>
2023-03-16 23:51:03 +0700

    commit

--- controllers/hospitals.js
+++ controllers/hospitals.js
@@ -1,4 +1,4 @@
-const Hosptial = require("../models/Hosptial");
+const Hosptial = require("../models/Hospital");
 
 /**
  * @desc    Get all hospitals
@@ -7,11 +7,73 @@ const Hosptial = require("../models/Hosptial");
  */
 exports.getHospitals = async (req, res, next) => {
   try {
-    const hospitals = await Hosptial.find();
-    res
-      .status(200)
-      .json({ success: true, count: hospitals.length, data: hospitals });
+    // Copy req.query
+    const reqQuery = { ...req.query };
+
+    // Fields to exclude
+    const removeFields = ["select", "sort"];
+
+    // Loop over removeFields and delete them from reqQuery
+    removeFields.forEach((param) => delete reqQuery[param]);
+
+    // Create operator ($gt, $gte, etc)
+    const queryStr = JSON.stringify(req.query).replace(
+      /\b(gt|gte|lt|lte|in)\b/g,
+      (match) => `$${match}`
+    );
+
+    // Finding resource
+    let query = Hosptial.find(JSON.parse(queryStr)).populate("appointments");
+
+    // Select Fields
+    if (req.query.select) {
+      const fields = req.query.select.split(",").join(" ");
+      query = query.select(fields);
+    }
+    // Sort
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort("-createdAt");
+    }
+    // Pagination
+    const page = parseInt(req.query.page, 10) || 1;
+    const limit = parseInt(req.query.limit, 10) || 25;
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+    const total = await Hosptial.countDocuments();
+
+    query = query.skip(startIndex).limit(limit);
+
+    // Executing query
+    const hospitals = await query;
+
+    // Pagination result
+    const pagination = {};
+
+    if (endIndex < total) {
+      pagination.next = {
+        page: page + 1,
+        limit,
+      };
+    }
+
+    if (startIndex > 0) {
+      pagination.prev = {
+        page: page - 1,
+        limit,
+      };
+    }
+
+    res.status(200).json({
+      success: true,
+      count: hospitals.length,
+      pagination,
+      data: hospitals,
+    });
   } catch (err) {
+    console.log(err);
     res.status(400).json({ success: false });
   }
 };
@@ -76,6 +138,7 @@ exports.deleteHospital = async (req, res, next) => {
     if (!hospital) {
       return res.status(400).json({ success: false });
     }
+    hospital.remove();
     res.status(200).json({ success: true, data: {} });
   } catch (err) {
     res.status(400).json({ success: false });
--- server.js
+++ server.js
@@ -11,6 +11,7 @@ connectDB();
 
 // Route files
 const hospitals = require("./routes/hospitals");
+const appointments = require("./routes/appointments");
 const auth = require("./routes/auth");
 
 const app = express();
@@ -20,6 +21,7 @@ app.use(cookieParser());
 
 // Mount routers
 app.use("/api/v1/hospitals", hospitals);
+app.use("/api/v1/appointments", appointments);
 app.use("/api/v1/auth", auth);
 
 const PORT = process.env.PORT || 5000;
